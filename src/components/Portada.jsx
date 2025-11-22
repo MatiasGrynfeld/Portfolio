c988cf7c503a2cbc2833116a9232a31e098c7fb5
MatiasGrynfeld <48519558@est.ort.edu.ar>
2025-11-22 15:07:19 -0300

    portada

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -3,6 +3,23 @@ import './Portada.css'
 function Portada() {
   return (
     <header className="portada">
+      <div className="logo">
+        <img src="/logo.png"/>
+      </div>
+      <h1 className="nombre">Matías Grynfeld</h1>
+      <p className="lema">Observar, Analizar, e Innovar</p>
+      <div className="iconos">
+        <a href="#" className="icono">
+          <img src="/githubIcon.png"/>
+        </a>
+        <a href="#" className="icono">
+          <img src="/linkedinIcon.png"/>
+        </a>
+        <a href="#" className="icono" id='CV_icon'>
+          CV
+        </a>
+      </div>
+      <button className="boton-empezar">Empezar</button>
     </header>
   )
 }
