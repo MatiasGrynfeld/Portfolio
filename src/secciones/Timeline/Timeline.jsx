f7acf18116209803ac4f13d172ab6e42973df769
MatiasGrynfeld <48519558@est.ort.edu.ar>
2025-11-22 23:55:18 -0300

    timeline

--- src/secciones/Timeline/Timeline.jsx
+++ src/secciones/Timeline/Timeline.jsx
@@ -2,13 +2,39 @@ import './Timeline.css'
 import Titulo from '../../components/Titulo'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
+import { useState, useEffect } from 'react'
 
 function Timeline() {
+  const [timelineData, setData] = useState({ elements: [] })
+
+  useEffect(() => {
+    fetch('/jsons/timeline.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error loading timeline.json:', error))
+  }, [])
+
   return (
     <section className="timeline">
       <Titulo texto="Educación y Experiencia" />
       <div className="timeline-wrapper">
         <VerticalTimeline>
+          {timelineData.elements.map((element) => (
+            <VerticalTimelineElement
+              key={element.id}
+              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            >
+              <h3 className="vertical-timeline-element-title">{element.title}</h3>
+              <h5 className='vertical-timeline-element-subtitle'>{element.date}</h5>
+              <p>{element.description}</p>
+              {element.button && (
+                <button className="timeline-button">
+                  <span className="boton-texto">{element.button}</span>
+                  <span className="boton-flechas">{">>"}</span>
+                </button>
+              )}
+            </VerticalTimelineElement>
+          ))}
         </VerticalTimeline>
       </div>
     </section>
